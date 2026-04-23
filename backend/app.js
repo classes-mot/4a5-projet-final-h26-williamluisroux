59a03453186cb5cb1bdae6fa9f3e3cedb20bcb59
William Luis Roux <williamluis567@gmail.com>
2026-04-23 12:11:49 -0400

    W.L.R : Complétion du code de app.js

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import usersRoutes from './routes/users-routes.js';
+import forumRoutes from './routes/forums-routes.js';
+import messagesRoutes from './routes/messages-routes.js'
+import errorHandler from './handler/error-handler.js';
+import { connectDB } from './util/bd.js';
+
+// Connexion à MongoDB
+await connectDB();
+
+const app = express();
+
+// Parse le code entrant pour ajouter une propriété body sur la request
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    ); //quel header sont autorisés ( pourait etre * pour tout)
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); // quelles methodes HTTP sont autorisées
+    next();
+});
+
+app.use('/api/users', usersRoutes);
+
+app.use('/api/forums', forumRoutes);
+
+app.use('/api/messages', messagesRoutes);
+
+
+app.use((req, res, next) => {
+    const error = new Error('Route non trouvée');
+    error.code = 404;
+    next(error);
+});
+
+app.use(errorHandler);
+
+app.listen(5000, () => {
+    console.log('serveur écoute au', `http://localhost:5000`);
+});
+
