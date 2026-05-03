07e5f0fa0e8200c6ee067d96fc1341d3e96ba5d6
William Luis Roux <williamluis567@gmail.com>
2026-05-03 19:44:09 -0400

    W.L.R. : Configuration vite pour les test unitaires

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  test: {
+    environment: 'jsdom', // Pour simuler un environnement DOM
+    globals: true, // Pour permettre l'utilisation de `describe`, `it`, `expect` sans les importer
+  },
+});
